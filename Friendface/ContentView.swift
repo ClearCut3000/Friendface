e9d1ed921cdee0c5f0dc70fb56c67ba16dc1193d
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-10-27 17:33:11 +0300

    Initial Commit

---
+++ Friendface/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Friendface
+//
+//  Created by Николай Никитин on 27.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Николай Никитин on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
